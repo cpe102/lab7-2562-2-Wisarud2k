e7912a47fd92eea5a8e9227478cc00e1ff70a7ac
test <xxx@google.com>
2020-01-21 15:34:30 +0700

    Done!!

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,tall,money;
+    string status;
+    cout << "Input your age: ";
+    cin >> age;
+    if(age<=20){
+        cout << "Input your Height: ";
+        cin >> tall;
+        if(tall<160){
+            status = "UNFRIEND";
+        }else if(tall<175){
+            status = "FRIEND";
+        }else{
+            cout << "Input your money: ";
+            cin >> money;
+            if(money>69){
+                status="MARRIED";
+            }else{
+                status="ONE-NIGHT-STAND";
+            }
+        }
+    }else if(age<30){
+        cout << "Input your money: ";
+        cin >> money;
+        if(money>10){
+            status="BEST FRIEND";
+        }else{
+            status="UNFRIEND";
+        }
+    }else{
+        status="UNFRIEND";
+    }
+    cout << "status = "<<status;
+}
+
 //status = "ONE-NIGHT-STAND";
